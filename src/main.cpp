9a11db45ed45cc8872e8204ba08faca37aeac4b2
SidorovichPavel <sidorovich.pasha@gmail.com>
2024-11-08 14:01:00 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <any>
+#include <concepts>
+#include <cstddef>
+#include <exception>
+#include <filesystem>
+#include <format>
+#include <functional>
+#include <future>
+#include <initializer_list>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <ranges>
+#include <span>
+#include <stack>
+#include <stacktrace>
+#include <stdexcept>
+#include <string_view>
+#include <type_traits>
+#include <typeinfo>
+#include <unordered_map>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#include <fmt/base.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+
+#include <boost/stacktrace.hpp>
+#include <boost/stacktrace/stacktrace.hpp>
+#include <boost/stacktrace/this_thread.hpp>
+
+auto main(int, char*[]) -> int {
+  std::ignore = 0;
+
+  return 0;
+}
